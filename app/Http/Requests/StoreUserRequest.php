e6ff3e8bd3f2a7faee85fab6772e3f9d2f578102
shashidas95 <shashidas95@gmail.com>
2023-05-28 22:52:06 +0600

    Request Validation

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -11,7 +11,7 @@ class StoreUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
-        ];
+        'name' => 'required|string|min:2',
+        'email' => 'required|email',
+         'password' => 'required|string|min:8',
+
+    ];
     }
 }
